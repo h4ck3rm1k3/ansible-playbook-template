62ade03324f731a5df0c3d0baa21d62c9ca37d4c
james mike dupont <jamesmikedupont@gmail.com>
2017-10-30 07:57:48 -0400

    better role support

--- create-playbook.py
+++ create-playbook.py
@@ -101,7 +101,7 @@ class TemplateTask():
 
         resultant = self._templar.do_template(data, preserve_trailing_newlines=True, escape_backslashes=False)
         self._templar.set_available_variables(old_vars)
-
+        print "test", self.dest
         o = open(self.dest,"w")
         o.write(resultant)
         o.close()
@@ -165,12 +165,37 @@ for a in roles:
     if not os.path.exists(r):
         os.mkdir(r)
 
+    r = "playbook/roles/{}/tasks".format(a)
+    if not os.path.exists(r):
+        os.mkdir(r)
+
+    r = "playbook/roles/{}/templates".format(a)
+    if not os.path.exists(r):
+        os.mkdir(r)
+
+    r = "playbook/roles/{}/".format(a)
+    
     task = TemplateTask(
         src='templates/role.yml',
         dest=r + "main.yml",
         task_vars = {
         })
-    r = task.run()
+    r3 = task.run()
+
+    task = TemplateTask(
+        src='templates/role_task.yml',
+        dest=r + "tasks/main.yml",
+        task_vars = {
+        })
+
+    r3 = task.run()
+
+    task = TemplateTask(
+        src='templates/role_template.yml',
+        dest=r + "templates/example.yml",
+        task_vars = {
+        })
+    r3 = task.run()
 
 hosts = {
     'all' : {
@@ -238,49 +263,6 @@ task = TemplateTask(
     task_vars = {    })
 r = task.run()
 
-
-# production                # inventory file for production servers
-# staging                   # inventory file for staging environment
-
-# group_vars/
-#    group1                 # here we assign variables to particular groups
-#    group2                 # ""
-# host_vars/
-#    hostname1              # if systems need specific variables, put them here
-#    hostname2              # ""
-
-# library/                  # if any custom modules, put them here (optional)
-# module_utils/             # if any custom module_utils to support modules, put them here (optional)
-# filter_plugins/           # if any custom filter plugins, put them here (optional)
-
-# site.yml                  # master playbook
-# webservers.yml            # playbook for webserver tier
-# dbservers.yml             # playbook for dbserver tier
-
-# roles/
-#     common/               # this hierarchy represents a "role"
-#         tasks/            #
-#             main.yml      #  <-- tasks file can include smaller files if warranted
-#         handlers/         #
-#             main.yml      #  <-- handlers file
-#         templates/        #  <-- files for use with the template resource
-#             ntp.conf.j2   #  <------- templates end in .j2
-#         files/            #
-#             bar.txt       #  <-- files for use with the copy resource
-#             foo.sh        #  <-- script files for use with the script resource
-#         vars/             #
-#             main.yml      #  <-- variables associated with this role
-#         defaults/         #
-#             main.yml      #  <-- default lower priority variables for this role
-#         meta/             #
-#             main.yml      #  <-- role dependencies
-#         library/          # roles can also include custom modules
-#         module_utils/     # roles can also include custom module_utils
-#         lookup_plugins/   # or other types of plugins, like lookup in this case
-
-#     webtier/              # same kind of structure as "common" was above, done for the webtier role
-#     monitoring/           # ""
-#     fooapp/               # ""
     
 
 #setattr(task,'args',args)
